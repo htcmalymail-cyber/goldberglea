fefda6a85b9c63eacf4aa6dce36749feb5d0edd4
htcmalymail-cyber <htcmaly.mail@gmail.com>
2026-04-23 19:43:40 +0300

    Add files via upload
    Ok

---
+++ users.js
@@ -0,0 +1,22 @@
+// 📌 БАЗА ЖИЛЬЦОВ И ДОСТУПОВ
+
+const USERS_DB = [
+  {
+    name: "Роман",
+    phone: "111",
+    apartment: "1",
+    role: "resident"
+  },
+  {
+    name: "Ирина",
+    phone: "222",
+    apartment: "2",
+    role: "resident"
+  },
+  {
+    name: "Куратор",
+    phone: "000",
+    apartment: "admin",
+    role: "admin"
+  }
+];
